7011f3f37bd3fbbe0d744084929ec85161c4859c
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-06 19:13:48 +0100

    second itineration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,29 +1,40 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
     this.direction = "up";
-    this.intervalID = [];
+    this.intervalID;
   }
 
   start() {
-    this.intervalID = setInterval(()=>this.update(),1*1000)
-}
+    this.intervalID = setInterval(() => {
+      this.floorUp();
+      this.update();
+    }, 1000);
+  }
+
   stop() {
     clearInterval(this.intervalID);
   }
   update() {
-  this.log() }
+    this.log();
+  }
 
 
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
+  _passengersEnter() {}
+  _passengersLeave() {}
+
+  floorUp() {
+    this.floor < this.MAXFLOOR ? this.floor++ : this.floor = this.floor;
+  }
+  floorDown() {
+    this.floor > 0 ? this.floor-- :this.floor = this.floor;
+  }
+  call() {}
   log() {
-  console.log(`directon ${this.direction} floor ${this.floor}`) }
+    console.log(`directon ${this.direction} floor ${this.floor}`)
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,4 +1,6 @@
 const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 let elevator = new Elevator ();
+
 elevator.update();
+elevator.start();
